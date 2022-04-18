767cb394c561875bd66a2ebc574376635a789fa2
Riley Paul <rileypaul96@gmail.com>
2022-04-18 10:26:39 -0600

    create head partial

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
+
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}...`));
