c4cdc1b6d9aba362854303a6666b85955085e29e
Riley Paul <rileypaul96@gmail.com>
2022-04-18 10:17:05 -0600

    create inital express server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
