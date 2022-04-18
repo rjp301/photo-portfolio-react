50c97c982e524c803d6a4a859b950bd316174ba1
Riley Paul <rileypaul96@gmail.com>
2022-04-18 10:44:28 -0600

    set up ejs template

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+const { resolveInclude } = require("ejs");
 const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}...`));
