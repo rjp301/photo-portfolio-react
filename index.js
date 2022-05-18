518ed0adf517824fd5f3581fccae362d3b5987f7
Riley Paul <rileypaul96@gmail.com>
2022-05-18 12:29:35 -0600

    Create about and contact page templates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { resolveInclude } = require("ejs");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -10,6 +11,14 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("pages/index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("pages/contact");
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about");
 });
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}...`));
